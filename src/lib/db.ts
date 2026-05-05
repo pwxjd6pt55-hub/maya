f53b6f15c10cf990cf40944f4302dca8ba63a846
pwxjd6pt55-hub <pwxjd6pt55@privaterelay.appleid.com>
2026-05-05 01:47:50 +0100

    lasr last commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: parseInt(process.env.DB_PORT || "3306"),  // ← ajouter cette ligne
+  host: process.env.DB_HOST || "tramway.proxy.rlwy.net",
+  port: parseInt(process.env.DB_PORT || "41725"),
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "railway",     // ← "railway" au lieu de "mayabar"
+  database: process.env.DB_NAME || "railway",
   waitForConnections: true,
   connectionLimit: 10,
 });
