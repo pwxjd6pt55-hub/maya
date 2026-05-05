e4aa710aa4fd53bfb44e6e9854caec3f29d98089
pwxjd6pt55-hub <pwxjd6pt55@privaterelay.appleid.com>
2026-05-05 01:50:04 +0100

    railways

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "tramway.proxy.rlwy.net",
   port: parseInt(process.env.DB_PORT || "41725"),
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "oyCHsEavgHVluryWCFnRZjhqJitaMEUN",
   database: process.env.DB_NAME || "railway",
   waitForConnections: true,
   connectionLimit: 10,
